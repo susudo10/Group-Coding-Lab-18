fcb260f8ae80f6b2c9bbf18ba7a234ac29e08298
mufaro-k07 <m.kunze@alustudent.com>
2025-06-02 03:52:04 -0700

    Menu to prompt the user

---
+++ archive_logs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# First creating the menu
+echo "Select the log to archive"
+echo "1) Heart Rate (heart_rate_log.log)"
+echo "2) Temperature (temperature_log.log)"
+echo "3) Water Usage (water_usage_log.log)"
+echo ""
+read -p "Enter the number of the log to archive(1,2 or 3): " user_log
